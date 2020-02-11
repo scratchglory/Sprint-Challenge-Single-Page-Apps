453b1e667d718d68e493e519fb41cc944a6d3a0d
adella <scratchglory@gmail.com>
2020-02-10 19:30:33 -0500

    added routes and search bar for locations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 import LocationList from "./components/LocationsList";
+
+import styled from "styled-components";
+
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: dodgerblue;
+  color: white;
+  border: 2px solid yellow;
+`;
 
 export default function App() {
   return (
     <main>
       <Header />
       <WelcomePage />
-      <CharacterList />
-      <LocationList />
+      <Button href="/">Home</Button>
+      <Button href="/character-list">Cast List</Button>
+      <Button href="/location-list">Locations</Button>
+
+      <Route path="/character-list" component={CharacterList} />
+      <Route path="/location-list" component={LocationList} />
     </main>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 import LocationCard from "./LocationCard";
+import SearchForm from "./SearchForm";
 
 export default function LocationsList() {
   const [location, setLocation] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
-      .get("https://rickandmortyapi.com/api/location/")
+      .get(`https://rickandmortyapi.com/api/location/?name=${search}`)
       .then(res => {
         setLocation(res.data.results);
       })
       .catch(err => {
         console.log(err);
       });
-  }, []);
+  }, [search]);
 
   return (
     <section>
       <h2>LOCATION</h2>
+      <div className="search-bar">
+        <Route
+          path="/"
+          render={props => (
+            <SearchForm {...props} setSearch={setSearch} search={search} />
+          )}
+        />
+      </div>
+
       {location.map(place => {
         return (
           <LocationCard
